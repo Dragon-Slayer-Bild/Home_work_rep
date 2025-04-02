089d0e973d795712c87b245946ca22eac9ca4a08
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-04-02 16:07:49 +0300

    lint fix

--- src/decorators.py
+++ src/decorators.py
@@ -3,20 +3,23 @@ import time
 
 def log(filename=None):
     """Декоратор, который логирует выполнение функции"""
+
     def my_decorator(function):
         def wrapper(*args, **kwargs):
-            try:
+            try:  # текст при выполнении функции
                 result = function(*args, **kwargs)
-                message = f'[{function.__name__}] result: {result} - OK [{time.asctime()}]\n'
-            except Exception as e:
-                message = f'[{function.__name__}] error: {e}. Inputs: {args}, {kwargs} [{time.asctime()}]\n'
+                message = f"[{function.__name__}] result: {result} - OK [{time.asctime()}]\n"
+            except Exception as e:  # текст при ошибке в выполнении функции
+                message = f"[{function.__name__}] error: {e}. Inputs: {args}, {kwargs} [{time.asctime()}]\n"
                 result = None
             finally:
-                if filename is None:
-                    print(message, end='')  # Важно: end='' чтобы не было лишнего переноса строки
-                else:
-                    with open(filename, 'a') as file: # 'a' для добавления в файл
+                if filename is None:  # вывод в консоль при отсутствии названия файла
+                    print(message, end="")
+                else:  # сохранение в файл при наличии названия файла
+                    with open(filename, "a") as file:
                         file.write(message)
             return result
+
         return wrapper
+
     return my_decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,49 +1,54 @@
-from src.decorators import log
 import pytest
+
+from src.decorators import log
 
 
 @pytest.fixture
 def sum_func():
     """Тестовая функция сложения, декорированная log с выводом в консоль"""
+
     @log()
     def inner_sum(x: int, y: int) -> int:
-        '''Реальная функция сложения'''
+        """Реальная функция сложения"""
         return x + y
+
     return inner_sum
 
 
 @pytest.fixture
 def sum_func_with_file():
     """Тестовая функция сложения, декорированная log с сохранением в файл."""
+
     @log(filename="mylog.txt")
     def inner_sum_with_file(x: int, y: int) -> int:
-        '''Реальная функция сложения'''
+        """Реальная функция сложения"""
         return x + y
+
     return inner_sum_with_file
 
 
 def test_sum_func_log(capsys, sum_func):
-    """Тест для проверки логирования функции sum_func."""
+    """Тест для проверки логирования функции с выводом в консоль."""
     result = sum_func(1, 2)
 
     assert result == 3
     captured = capsys.readouterr()
-    assert '[inner_sum] result: 3 - OK' in captured.out
+    assert "[inner_sum] result: 3 - OK" in captured.out
 
 
 def test_sum_func_log_with_file(sum_func_with_file):
-    """Тест для проверки логирования функции sum_func."""
+    """Тест для проверки логирования функции с сохранением в файл."""
     result = sum_func_with_file(1, 4)
 
     assert result == 5
-    with open('mylog.txt', "r") as f:
+    with open("mylog.txt", "r") as f:
         log_text = f.read()
 
     assert "[inner_sum_with_file] result: 5 - OK" in log_text
 
 
 def test_sum_func_log_error(capsys, sum_func):
-    """Тест для проверки логирования функции sum_func."""
+    """Тест для проверки логирования функции при ошибке с выводом в консоль."""
     try:
         sum_func(1)
     except TypeError:
@@ -53,6 +58,14 @@ def test_sum_func_log_error(capsys, sum_func):
     assert "missing 1 required positional argument: 'y'" in captured.out
 
 
+def test_sum_func_log_error_with_file(sum_func_with_file):
+    """Тест для проверки логирования функции при ошибке с сохранением в файл."""
+    try:
+        sum_func_with_file(1)
+    except TypeError:
+        pass
 
+    with open("mylog.txt", "r") as f:
+        log_text = f.read()
 
-
+    assert "missing 1 required positional argument: 'y'" in log_text
